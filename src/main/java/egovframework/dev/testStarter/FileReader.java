569e12f2323d46e72537bb1ba4c72901ab6f677a
minusstory <plusitstory@skysoft.co.kr>
2015-05-14 13:30:27 +0900

    자료 추가 및 소스 추가

---
+++ src/main/java/egovframework/dev/testStarter/FileReader.java
@@ -0,0 +1,7 @@
+package egovframework.dev.testStarter;
+
+class FileReader{
+    public static void main(String args[]) {
+        // TODO Auto-generated method stub
+    }
+}
---
+++ src/main/java/egovframework/dev/testStarter/SkysoftAddr.java
@@ -0,0 +1,10 @@
+package egovframework.dev.testStarter;
+
+class SkysoftAddr {
+    private static String[][] addrArr = {
+        // TODO Auto-generated method stub
+    };
+    public static void main(String args[]) {
+        // TODO Auto-generated method stub
+    }
+}
